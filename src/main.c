f6a3c83abd8f7a80d4063146f3409f7493c70656
Eiyeron Fulmincendii <eiyeron@retroactive.me>
2022-04-29 01:00:02 +0200

    Misc optimizations
    - Precomputing the tunnel's profile/scaling
      - This was an easy optimization. `sin` is probably not that cheap,
        even on Cortex-M7.
    - Removal of call to `clearScreen` (as the whole screen is drawn to
      anyway)
      - This involves bringing back the call of `markUpdatedRows` as
        `clearScreen` marked itself the whole screen to be updated.
    - Change of a call to fmod into `truncf`
      - `truncf` seems to map directly to the opcode `vrintz` instead of a
        call to a supprot function. So I suppose it runs faster while doing
        the same operation.
    - Changes some variables into `uint32_t`
      - Now we're grasping for spare cycles which are maybe not even
        actually there. Some architectures had a distinct performance impact
        by using non-word-sized types. but we're talking about a recent CPU,
        so...
    - Possible optimization doable on output bit packing kept in TODO.
      - Because this specific bit was done when I was the most tired, the
      involved math was a little questionable. I kept a TODO for future
      comparison. There might also be a way to only update the selected bit
      when the input pointer/cursor moves (use the `integer_part` to
      shift-left the input byte and take the leftmost bit?)
    
     Thanks to timhei for giving performance report on the PlayDate Discord
     servier!

--- src/main.c
+++ src/main.c
@@ -10,7 +10,12 @@
 LCDBitmap* bitmap;
 LCDBitmap* gradient;
 
+int gradient_width = 0;
+int gradient_height = 0;
+
 static char error_message[256] = "";
+
+float tunnel_profile[LCD_ROWS] = { 0.f };
 
 static int crash_handler(void* userdata)
 {
@@ -49,19 +54,24 @@ static bool init(PlaydateAPI* pd)
         return false;
     }
 
-    int width, height;
-    int rowbytes;
-    int mask;
-    uint8_t* data;
-    pd->graphics->getBitmapData(bitmap, &width, &height, &rowbytes, &mask, &data);
+    pd->graphics->getBitmapData(gradient, &gradient_width, &gradient_height, NULL, NULL, NULL);
+
+    // Precomputing the cosine effect to improve performance
+
+    for (int y = 0; y < LCD_ROWS; ++y)
+    {
+        // Bitmap scale, that's the core the of effect. 
+        // Feel free to tweak the factors to see how it changes but
+        // note that the effect currently doesn't work with negative scale.
+        tunnel_profile[y] = sinf(y / 240.f * 3.141592653f) * 0.55f + 0.75f;
+    }
+
     return true;
 }
 
 static int update(void* userdata)
 {
     PlaydateAPI* pd = userdata;
-
-    pd->graphics->clear(kColorWhite);
 
     int width, height;
     int rowbytes;
@@ -96,14 +106,10 @@ static int update(void* userdata)
     for (int y = 0; y < 240; ++y)
     {
 
-        // Bitmap scale, that's the core the of effect. 
-        // The scale and offset values could be hardcoded into arrays.
-        // Feel free to tweak the factors to see how it changes but
-        // note that the effect currently doesn't work with negative scale.
-        float scale = sinf(y / 240.f * 3.141592653f) * 0.55f + 0.75f;
         // The scale-based offset makes the effect centered.
         // Try running without the offset, it'll look like it's centered on the
         // left-most pixel column.
+        float scale = tunnel_profile[y];
         float base_x_offset = x_offset - scale * 200.f;
 
 
@@ -127,16 +133,16 @@ static int update(void* userdata)
         // a register.
         int input_y_row_offset = input_y * rowbytes;
         uint8_t* input_pointer = &data[input_y_row_offset + input_byte_offset];
-        uint8_t input_byte = *input_pointer;
+        uint32_t input_byte = *input_pointer;
 
         uint8_t* output_pointer = &buffer[y * LCD_ROWSIZE];
         // Temporary byte to work on until I blit it directly to the framebuffer.
         // Might be purely useless, maybe optimized out. I need to tune this too.
-        uint8_t output_acc = 0;
+        uint32_t output_acc = 0;
 
         // This could be split into double for loops instead,
         // for(x; x < LCD_COLUMNS; x+=8) for(bit; bit < 8; ++bit)
-        uint8_t written_bits = 0;
+        uint32_t written_bits = 0;
 
         // A runtime hint for the CPU to load the next cacheline-width data at the adress.
         // I'm not sure if it has a performance impact, that's one of the things I want to check
@@ -151,8 +157,9 @@ static int update(void* userdata)
             // If the input must be advanced
             if (frac >= 1)
             {
-                input_bit_pointer += (uint8_t)truncf(frac);
-                frac = fmodf(frac, 1.f);
+                float integer_part = truncf(frac);
+                input_bit_pointer += (uint32_t)integer_part;
+                frac -= integer_part;
                 // The current input byte is through, fetch the next one.
                 if (input_bit_pointer >= 8)
                 {
@@ -160,9 +167,9 @@ static int update(void* userdata)
                     // the compiler is smart enough to figure it out.
                     //
                     // Update: GCC is smart enough to do the change.
-                    int offset = input_bit_pointer / 8;
+                    uint32_t offset = input_bit_pointer / 8;
                     input_bit_pointer %= 8;
-                    input_byte_offset = (uint8_t)((input_byte_offset + offset) % rowbytes);
+                    input_byte_offset = (input_byte_offset + offset) % rowbytes;
                     input_pointer = &data[input_y_row_offset + input_byte_offset];
                     input_byte = *input_pointer;
                 }
@@ -171,15 +178,22 @@ static int update(void* userdata)
             // Output data seek and write.
             // Tricky bit : Because the data is aligned as MSB data, we must take care of the way
             // we collect and apply bits. From left to right => from the MSB to the LSB.
-            uint8_t selected_bit = (input_byte & (1 << (7 - input_bit_pointer))) != 0;
+            uint32_t selected_bit = (input_byte & (1 << (7 - input_bit_pointer))) != 0;
 
+            // TODO Does this works faster than this weird operation?
+            // It should work the same, pushing the bits from the biggest to the smallest
+            // but we'retalking about a few cycles of difference.
+            /*
+            output_acc <<= 1;
+            output_acc |= selected_bit;
+            */
             output_acc |= selected_bit << (7 - written_bits);
             ++written_bits;
 
             // Write to the screen once we got a full byte.
             if (written_bits == 8)
             {
-                *output_pointer = output_acc;
+                *output_pointer = (uint8_t)output_acc;
                 ++output_pointer;
                 output_acc = 0;
                 written_bits = 0;
@@ -194,9 +208,6 @@ static int update(void* userdata)
     // but I wanted to see how some of the gradients would look without having to
     // program them.
     // I bundled a few different gradients if you want to test them.
-    int gradient_width = 0;
-    int gradient_height = 0;
-    pd->graphics->getBitmapData(gradient, &gradient_width, &gradient_height, NULL, NULL, NULL);
     if (gradient_width != 0)
     {
         for (int x = 0; x < LCD_COLUMNS; x += gradient_width)
@@ -205,9 +216,9 @@ static int update(void* userdata)
         }
     }
 
-    // I'm not sure if it's really needed, given that I directly tap into the framebuffer.
-    //pd->graphics->markUpdatedRows(0, LCD_ROWS - 1);
-    
+    // Because there's not a single graphics routine used other than the draw fps one
+    // we have to declare we want the whole screen updated.
+    pd->graphics->markUpdatedRows(0, LCD_ROWS - 1);
     
     pd->system->drawFPS(0, 0);
     return 1;
